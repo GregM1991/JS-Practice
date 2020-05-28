cfb7f6e9d0e6b63c969d9733ca8e9ffd726e128d
Greg Martin <gregm31@live.com>
2020-05-28 15:29:51 +1000

    Playing with keypresses and inputs, made crude shopping list

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const colors = [ 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'indigo', 'violet' ];
 
 const container = document.querySelector('#boxes');
+const input = document.querySelector('#username');
+const addItemInput = document.querySelector('#addItem');
+const itemsUL = document.querySelector('#items');
 
-const printColor = function() {
+input.addEventListener('keydown', function(e) {
+	console.log('down');
+});
+input.addEventListener('keyup', function(e) {
+	console.log('up');
+});
+
+const printColor = function(evt) {
+	console.log(evt);
 	const h1 = document.querySelector('h1');
 	h1.style.color = this.style.backgroundColor;
 	console.log(this.style.backgroundColor);
@@ -15,3 +26,14 @@ for (let color of colors) {
 	container.appendChild(box);
 	box.addEventListener('click', printColor);
 }
+
+addItemInput.addEventListener('keypress', function(e) {
+	if (e.key === 'Enter') {
+		if (!this.value) return;
+		const newItemText = this.value;
+		const newItem = document.createElement('li');
+		newItem.innerText = newItemText;
+		itemsUL.appendChild(newItem);
+		this.value = '';
+	}
+});
