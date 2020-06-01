c0aca33057da0101c725cfc77c37fabd7baeefac
Greg Martin <gregm31@live.com>
2020-06-01 15:32:46 +1000

    Using the Axios HTTP client and refactoring code

--- ajax.js
+++ ajax.js
@@ -15,19 +15,42 @@ console.log('hellow world');
 // firstReq.send();
 // console.log('Request sent');
 
-fetch('https://swapi.dev/api/planetssad21/')
-	.then((response) => {
-		if (!response.ok) {
-			throw new Error(`Status Code Error: ${response.status}`);
-		} else {
-			response.json().then((data) => {
-				for (let planet of data.results) {
-					console.log(planet.name);
-				}
-			});
-		}
-	})
+// fetch('https://swapi.dev/api/planetssad21/')
+// 	.then((response) => {
+// 		if (!response.ok) {
+// 			throw new Error(`Status Code Error: ${response.status}`);
+// 		} else {
+// 			response.json().then((data) => {
+// 				for (let planet of data.results) {
+// 					console.log(planet.name);
+// 				}
+// 			});
+// 		}
+// 	})
+// 	.catch((err) => {
+// 		console.log('Something went wrong');
+// 		console.log(err);
+// 	});
+
+const fetchNextPlanets = (url = 'https://swapi.dev/api/planets/') => {
+	return axios.get(url);
+};
+
+const printPlanets = ({ data }) => {
+	for (let planet of data.results) {
+		console.log(planet.name);
+	}
+	return Promise.resolve(data.next);
+};
+
+fetchNextPlanets()
+	.then(printPlanets)
+	.then(fetchNextPlanets)
+	.then(printPlanets)
+	.then(fetchNextPlanets)
+	.then(printPlanets)
+	.then(fetchNextPlanets)
+	.then(printPlanets)
 	.catch((err) => {
-		console.log('Something went wrong');
-		console.log(err);
+		console.log('ERROR', err);
 	});
