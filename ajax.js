b56bdba2a3a42fa7fdbd4354b00e93a2a9865a7f
Greg Martin <gregm31@live.com>
2020-06-01 14:45:37 +1000

    Refactored XMLHttpRequest with fetch API

--- ajax.js
+++ ajax.js
@@ -1,16 +1,33 @@
 console.log('hellow world');
 
-const firstReq = new XMLHttpRequest();
-firstReq.addEventListener('load', function() {
-	console.log('IT WORKED');
-	const data = JSON.parse(this.responseText);
-	for (let planet of data.results) {
-		console.log(planet.name);
-	}
-});
-firstReq.addEventListener('error', () => {
-	console.log('ERROR!!!');
-});
-firstReq.open('GET', 'https://swapi.dev/api/planets/');
-firstReq.send();
-console.log('Request sent');
+// const firstReq = new XMLHttpRequest();
+// firstReq.addEventListener('load', function() {
+// 	console.log('IT WORKED');
+// 	const data = JSON.parse(this.responseText);
+// 	for (let planet of data.results) {
+// 		console.log(planet.name);
+// 	}
+// });
+// firstReq.addEventListener('error', () => {
+// 	console.log('ERROR!!!');
+// });
+// firstReq.open('GET', 'https://swapi.dev/api/planets/');
+// firstReq.send();
+// console.log('Request sent');
+
+fetch('https://swapi.dev/api/planetssad21/')
+	.then((response) => {
+		if (!response.ok) {
+			throw new Error(`Status Code Error: ${response.status}`);
+		} else {
+			response.json().then((data) => {
+				for (let planet of data.results) {
+					console.log(planet.name);
+				}
+			});
+		}
+	})
+	.catch((err) => {
+		console.log('Something went wrong');
+		console.log(err);
+	});
